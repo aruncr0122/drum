54485b5712addf8f9c99d48575fa5ebcd165efd9
aruncr0122 <85609397+aruncr0122@users.noreply.github.com>
2021-06-19 10:33:07 +0530

    musical instrument

---
+++ index.js
@@ -0,0 +1,54 @@
+
+//button events
+var length = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var ch = this.innerHTML;
+        makesound(ch);
+        btnAnimation(ch);
+    })
+
+}
+
+//keyboard events
+document.addEventListener("keypress", function (e) {
+    makesound(e.key);
+    btnAnimation(e.key);
+})
+
+function makesound(key) {
+
+    switch (key) {
+        case "w": var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a": var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s": var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d": var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j": var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "k": var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l": var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        default: console.log(ch);
+    }
+}
+
+function btnAnimation(current) {
+    var activebtn = document.querySelector("." + current);
+    activebtn.classList.add("pressed");
+    setTimeout(function () {
+        activebtn.classList.remove("pressed");
+    }, 100)
+}
